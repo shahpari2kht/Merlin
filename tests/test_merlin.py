5c4eab131d0c90f13b89ffce5f994b73d47b73c1
Parisa Mohammadzadeh <110891110+shahpari2kht@users.noreply.github.com>
2025-11-10 01:37:21 +0330

    Update test_merlin.py

--- tests/test_merlin.py
+++ tests/test_merlin.py
@@ -1,2 +1,8 @@
-def test_sanity():
-    assert 2 + 2 == 4
+import pytest
+from src import merlin
+
+def test_clean_basic():
+    import pandas as pd
+    df = pd.DataFrame({"A": [1, None, 3]})
+    df_clean = merlin.clean(df)
+    assert df_clean.isnull().sum().sum() == 0
